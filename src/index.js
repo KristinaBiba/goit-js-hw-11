3b9f7c84efaa3047f1078952107b159f5d58ab8a
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2022-11-18 22:34:01 +0200

    part-1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+const axios = require('axios').default;
 
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
@@ -10,53 +12,76 @@ formEl.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-
+  galleryEl.innerHTML = '';
   keyword = e.target.elements.searchQuery.value;
-
+  // console.log(keyword);
   findPhoto()
     .then(photos => {
       if (photos.length === 0) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
-          );
-          createGallery(photos);
-        console.log(photos);
+        );
+        return;
       }
+      console.log(photos);
+      createGallery(photos);
     })
     .catch(error => console.log);
 
-    function createGallery(photosArray) {
-        photosArray.map({webformatUR,largeImageURL,tags,likes,views,comments,downloads} => {
-                  // <div class="photo-card">
-    //   <img src="${webformatUR}" alt="${tags}" loading="lazy" />
-    //   <div class="info">
-    //     <p class="info-item">
-    //       <b>Likes</b>${likes}
-    //     </p>
-    //     <p class="info-item">
-    //       <b>Views</b>${views}
-    //     </p>
-    //     <p class="info-item">
-    //       <b>Comments</b>${comments}
-    //     </p>
-    //     <p class="info-item">
-    //       <b>Downloads</b>${downloads}
-    //     </p>
-    //   </div>
-    // </div>;
-      })
-
+  function createGallery(photosArray) {
+    const photoObject = photosArray
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<a class="photo-card" href=${largeImageURL}>
+          <img src=${webformatURL} alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b> ${downloads}
+            </p>
+          </div>
+        </a>`
+      )
+      .join('');
+    galleryEl.innerHTML = photoObject;
+    console.log(photoObject);
   }
-
-  //   
-
-  //   console.log(findPhoto());
 }
-
 const findPhoto = async () => {
-  const response = await fetch(
-    `${MAIN_URL}?key=${KEY_API}&q=${keyword}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  const photos = await response.json();
-  return photos.hits;
+  try {
+    const response = await axios.get(`${MAIN_URL}`, {
+      params: {
+        key: KEY_API,
+        q: keyword,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+      },
+    });
+    return response.data.hits;
+  } catch (error) {
+    console.error(error);
+  }
+  // const response = await fetch(
+  //   `${MAIN_URL}?key=${KEY_API}&q=${keyword}&image_type=photo&orientation=horizontal&safesearch=true`
+  // );
+  // const photos = await response.json();
+  // console.log(photos);
+  // return photos.hits;
 };
