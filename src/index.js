b2b081fcad0e2aecc5f138e78a78d8519fca2784
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2022-11-19 22:26:39 +0200

    task-2

--- src/index.js
+++ src/index.js
@@ -1,45 +1,40 @@
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from '../node_modules/simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const axios = require('axios').default;
 
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadBtnEl = document.querySelector('.load-more');
 
 const MAIN_URL = 'https://pixabay.com/api/';
 const KEY_API = '31406001-9559e1c679da811c7e2a75baf';
 let keyword = '';
+let currentPage = 0;
 formEl.addEventListener('submit', onSubmit);
+loadBtnEl.addEventListener('click', onLoadPhoto);
 
 function onSubmit(e) {
   e.preventDefault();
   galleryEl.innerHTML = '';
+  currentPage = 0;
   keyword = e.target.elements.searchQuery.value;
-  // console.log(keyword);
-  findPhoto()
-    .then(photos => {
-      if (photos.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      console.log(photos);
-      createGallery(photos);
-    })
-    .catch(error => console.log);
+  onLoadPhoto();
 
-  function createGallery(photosArray) {
-    const photoObject = photosArray
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => `<a class="photo-card" href=${largeImageURL}>
+  // console.log(keyword);
+}
+function createGallery(photosArray) {
+  const photoObject = photosArray
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<a class="photo-card" href=${largeImageURL}>
           <img src=${webformatURL} alt="${tags}" loading="lazy" />
           <div class="info">
             <p class="info-item">
@@ -56,13 +51,35 @@ function onSubmit(e) {
             </p>
           </div>
         </a>`
-      )
-      .join('');
-    galleryEl.innerHTML = photoObject;
-    console.log(photoObject);
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', photoObject);
+  const gallery = new SimpleLightbox('.gallery a');
+}
+
+function onLoadPhoto() {
+  currentPage += 1;
+
+  findPhoto()
+    .then(photos => {
+      createGallery(photos);
+    })
+    .catch(error => console.log);
+  if (currentPage > 1) {
+    const { height: cardHeight } =
+      galleryEl.lastElementChild.getBoundingClientRect();
+    window.scrollBy(0, cardHeight * 2);
+    // {
+    //   top: cardHeight * 2,
+    //   behavior: 'smooth',
+    // }
   }
 }
-const findPhoto = async () => {
+
+async function findPhoto() {
+  loadBtnEl.classList.add('hidden');
+
   try {
     const response = await axios.get(`${MAIN_URL}`, {
       params: {
@@ -72,16 +89,31 @@ const findPhoto = async () => {
         orientation: 'horizontal',
         safesearch: true,
         per_page: 40,
+        page: currentPage,
       },
     });
+    if (response.data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    if (currentPage === 1) {
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.totalHits} images.`
+      );
+    }
+
+    if (currentPage * 40 > response.data.totalHits) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return response.data.hits;
+    }
+
+    loadBtnEl.classList.remove('hidden');
     return response.data.hits;
   } catch (error) {
     console.error(error);
   }
-  // const response = await fetch(
-  //   `${MAIN_URL}?key=${KEY_API}&q=${keyword}&image_type=photo&orientation=horizontal&safesearch=true`
-  // );
-  // const photos = await response.json();
-  // console.log(photos);
-  // return photos.hits;
-};
+}
